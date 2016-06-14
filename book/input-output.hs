2f969bd86a709232889f297f8c0a4a3e2e27ea1c
Daniel Guimaraes <dcguim@gmail.com>
2016-06-14 00:06:59 -0300

    Finished the first part of I/O, now going to streams

--- book/input-output.hs
+++ book/input-output.hs
@@ -50,16 +50,16 @@
 -- to bind pure expressions to names we use let
 -- as: let firstName = map toUpper firstName.
 
-main = do
-  line <- getLine
-  if null line
-     then return ()
-    else (do
-          putStrLn $ reverseWords line
-          main)
+-- main = do
+--  line <- getLine
+--  if null line
+--     then return ()
+--    else (do
+--          putStrLn $ reverseWords line
+--          main)
     
-reverseWords :: String -> String
-reverseWords = unwords . map reverse . words
+--reverseWords :: String -> String
+--reverseWords = unwords . map reverse . words
 
 -- Three important points must be made about I/O actions:
 
@@ -81,3 +81,68 @@ reverseWords = unwords . map reverse . words
 -- to create an I/O action that doesn`t do anything
 -- or when its necessary to return the return a value
 -- of a do block which is not the last action.
+
+-- putStr function, putStr:: String -> IO ()
+-- main = do putStr "Hey, "
+--          putStr "I`m "
+--          putStrLn "Andy!"
+-- the only diff. from putStrLn is that it doesn`t
+-- jump a line. It could be defined in terms of
+-- putChar which takes a character and returns an I/O action
+
+-- putStr':: String -> IO ()
+-- putStr' [] = return ()
+-- putStr' (x:xs) = do
+--  putChar x
+--  putStr' xs
+
+-- main = do putStr' "Hello Folks!"
+
+-- import Data.Char  
+
+-- function putChar reads a character from input (getChar:: IO Char)
+-- and the result contained within the I/O action is a Char.
+-- main = do
+--   c <- getChar
+--   if c /= ' '
+--      then (do
+--            putChar c
+--            main)
+--     else return ()
+
+-- when function takes a boolean value and an I/O action
+-- if that boolean is True it returns the same I/O action
+
+-- import Control.Monad
+
+-- main = do
+--  c <- getChar
+--  when (c /= ' ') $ do
+--    putChar c
+--    main
+
+-- Sequence takes a list of I/O actions and return
+-- an I/O action that will perform those actions one
+-- after the other. The result contained in that I/O
+-- action will be a list of results of all I/O actions
+-- of all I/O actions that were performed.
+-- main = do
+--  rs <- sequence [getLine, getLine, getLine]
+--  print rs
+
+-- It is common to map functions like print or putStrLn
+-- aka I/O actions over lists: sequence (map print [1,2,3,4])
+-- But there`s a utility function mapM and mapM_ that do the job
+-- mapM_ can be used when we are not interested in the result.
+
+import Control.Monad  
+  
+main = do   
+  colors <- forM [1,2,3,4]
+            (\a -> do  
+                putStrLn $ "Which color do you associate with the number " ++ show a ++ "?"  
+                color <- getLine  
+                return color)  
+  putStrLn "The colors that you associate with 1, 2, 3 and 4 are: "  
+  mapM putStrLn colors  
+
